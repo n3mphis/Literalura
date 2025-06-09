3fa43845cba029768191d442f4b067e10e71c3e9
N3mphis <66238416+n3mphis@users.noreply.github.com>
2025-06-09 10:11:35 -0300

    Creado el paquete model donde cree la clase para consumir la API

---
+++ src/main/java/com/Literalura/model/ConsumoAPI.java
@@ -0,0 +1,23 @@
+package com.Literalura.model;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoAPI {
+    public String obtenerDatos(String url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String json = response.body();
+        return json;
+    }
+}
